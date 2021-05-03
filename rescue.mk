21158325ed4765c38f5b286e6af4b09c20c5b65d
Daniel Palmer <daniel@0x0f.com>
2021-05-03 21:42:30 +0900

    Targets for rescue images

---
+++ rescue.mk
@@ -0,0 +1,27 @@
+BUILDROOT_RESCUE_ARGS += BR2_DL_DIR=$(DLDIR) BR2_EXTERNAL="$(EXTERNALS)"
+
+ifdef DEFCONFIG_RESCUE
+	BUILDROOT_RESCUE_ARGS += BR2_DEFCONFIG="$(DEFCONFIG_RESCUE)"
+endif
+
+.PHONY: buildroot_rescue
+
+buildroot-rescue: $(OUTPUTS) $(DLDIR)
+# Buildroot generates so much output drone ci can
+# handle it, so tell make to be quiet
+	$(MAKE) -s -C buildroot_rescue $(BUILDROOT_RESCUE_ARGS)
+
+# For CI caching. Download all of the source so you
+# can cache it and reuse it for then next build
+buildroot-rescue-dl: $(OUTPUTS) $(DLDIR)
+	$(MAKE) -C buildroot_rescue $(BUILDROOT_RESCUE_ARGS) defconfig
+	$(MAKE) -C buildroot_rescue $(BUILDROOT_RESCUE_ARGS) source
+
+buildroot-rescue-menuconfig:
+	$(MAKE) -C buildroot_rescue $(BUILDROOT_RESCUE_ARGS) menuconfig
+
+buildroot-rescue-savedefconfig:
+	$(MAKE) -C buildroot_rescue $(BUILDROOT_RESCUE_ARGS) savedefconfig
+
+buidroot-rescue-clean:
+	$(MAKE) -C buildroot_rescue $(BUILDROOT_RESCUE_ARGS) clean
